d0a278d137a699d7b0cdec0a24c0610bfcac3771
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2023-10-06 19:29:34 +0530

    Adding the Directory

--- js/index.js
+++ js/index.js
@@ -69,3 +69,4 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
